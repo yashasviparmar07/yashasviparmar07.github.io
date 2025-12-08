d1804535c0739fe5a26072183fedddc7206f8dbe
yashasviparmar07 <yashasviparmar07@gmail.com>
2025-12-08 00:38:32 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', function(e){
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
-
